e9d003c3a83559ca1f36e1b89ec67287ac07e630
Kamal <kamalaitsidhoum@gmail.com>
2020-04-13 13:56:24 -0400

    added dependency JUnit

--- src/main/java/com/tier1/driver/Driver.java
+++ src/main/java/com/tier1/driver/Driver.java
@@ -1,8 +1,11 @@
 package com.tier1.driver;
 
+import com.tier1.model.User;
+
 public class Driver {
   
   static int[] integerArray = new int[] {1,4,6,7,4};
+  User user = new User();
   
   public static void main(String[] args) {
    System.out.println(sumArray(integerArray));
